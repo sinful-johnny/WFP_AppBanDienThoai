f17130e3d3a23462a300613aa55b5dc19f983191
sinful_johnny <megaVNN321123@gmail.com>
2024-03-13 12:52:53 +0700

    add login screen

---
+++ HW4/HW4/LoginScreen.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW4
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        public SqlConnection _connection;
+        void Encrypt(string password, string username)
+        {
+            var passwordInBytes = Encoding.UTF8.GetBytes(password);
+            var entropy = new byte[20];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(entropy);
+            }
+            var cypherText = ProtectedData.Protect(passwordInBytes, entropy, DataProtectionScope.CurrentUser);
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["username"].Value = username;
+            config.AppSettings.Settings["password"].Value = Convert.ToBase64String(cypherText);
+            config.AppSettings.Settings["entropy"].Value = Convert.ToBase64String(entropy);
+            config.AppSettings.Settings["isPasswordRemmembered"].Value = "1";
+            config.Save(ConfigurationSaveMode.Minimal);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                string password = PasswordBox.Password;
+                string username = UsernameTextBox.Text;
+                string server = ConfigurationManager.AppSettings["Server"];
+                string Database = ConfigurationManager.AppSettings["Database"];
+
+                string connectionString = $"""
+                                              Server={server};
+                                              Database={Database};
+                                              User ID= {username};
+                                              Password= {password}; 
+                                              TrustServerCertificate=True
+                                              """;
+                _connection = new SqlConnection(connectionString);
+                _connection.Open();
+                if(_connection.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Good!", "Logged in successfully", MessageBoxButton.OK);
+                    if (RemembermeCheckBox.IsChecked == true)
+                    {
+                        Encrypt(password, username);
+                    }
+                    else
+                    {
+                        config.AppSettings.Settings["isPasswordRemmembered"].Value = "0";
+                        config.AppSettings.Settings["username"].Value = " ";
+                        config.AppSettings.Settings["password"].Value = " ";
+                        config.AppSettings.Settings["entropy"].Value = " ";
+                        config.Save(ConfigurationSaveMode.Minimal);
+                    }
+                    DialogResult = true;
+                }else
+                {
+                    MessageBox.Show("Wrong credential!", "Log in failed", MessageBoxButton.OK);
+                }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings["isPasswordRemmembered"].Value == "1")
+            {
+                UsernameTextBox.Text = config.AppSettings.Settings["username"].Value;
+                var cypherText = Convert.FromBase64String(ConfigurationManager.AppSettings["password"]);
+                var entropy = Convert.FromBase64String(ConfigurationManager.AppSettings["entropy"]);
+                var decryptedPassword = ProtectedData.Unprotect(cypherText, entropy, DataProtectionScope.CurrentUser);
+                var realPassword = Encoding.UTF8.GetString(decryptedPassword);
+
+                PasswordBox.Password = realPassword;
+                RemembermeCheckBox.IsChecked = true;
+            }
+        }
+    }
+}
--- QLDTHOAI.sql
+++ QLDTHOAI.sql
@@ -5,6 +5,7 @@ go;
 
 use QLDTHOAI;
 
+--drop table PHONE
 create table PHONE(
 	ID int identity(1,1) PRIMARY KEY,
 	NAME varchar(50),
@@ -37,4 +38,26 @@ values	('W41',(select ID from MANUFACTURER where NAME = 'LG'),'./Images/Phone00.
 		('iPhone SE (2022)',(select ID from MANUFACTURER where NAME = 'Apple'),'./Images/Phone06.jpg',669),
 		('Samsung Galaxy S24 Ultra',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone07.jpg',669),
 		('Samsung Galaxy Xcover7',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone08.jpg',669),
-		('Samsung Galaxy Tab S9 FE+',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone09.jpg',669)
+		('Samsung Galaxy Tab S9 FE+',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone09.jpg',669),
+		('Samsung Galaxy Tab S10 E-',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone09.jpg',669),
+		('iPhone 4 S Pro Max+',(select ID from MANUFACTURER where NAME = 'Apple'),'./Images/Phone09.jpg',669),
+		('Note 11',(select ID from MANUFACTURER where NAME = 'Xiaomi'),'./Images/Phone09.jpg',669),
+		('Note 10',(select ID from MANUFACTURER where NAME = 'Xiaomi'),'./Images/Phone09.jpg',669),
+		('iPhone 6S',(select ID from MANUFACTURER where NAME = 'Apple'),'./Images/Phone09.jpg',669),
+		('iPad 4',(select ID from MANUFACTURER where NAME = 'Apple'),'./Images/Phone09.jpg',669),
+		('Samsung Galaxy S23 Ultra',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone09.jpg',669),
+		('Samsung Galaxy S22 Ultra',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone09.jpg',669),
+		('Samsung Galaxy S21',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone09.jpg',669),
+		('iPhone 4S',(select ID from MANUFACTURER where NAME = 'Apple'),'./Images/Phone09.jpg',669),
+		('iPhone 6+',(select ID from MANUFACTURER where NAME = 'Apple'),'./Images/Phone09.jpg',669),
+		('K64',(select ID from MANUFACTURER where NAME = 'LG'),'./Images/Phone01.jpg',69.69),
+		('OLE86',(select ID from MANUFACTURER where NAME = 'LG'),'./Images/Phone01.jpg',69.69),
+		('Q24',(select ID from MANUFACTURER where NAME = 'LG'),'./Images/Phone01.jpg',69.69),
+		('Note 7+',(select ID from MANUFACTURER where NAME = 'Xiaomi'),'./Images/Phone09.jpg',669),
+		('Note 11+',(select ID from MANUFACTURER where NAME = 'Xiaomi'),'./Images/Phone09.jpg',669),
+		('Gamming Phone 2022',(select ID from MANUFACTURER where NAME = 'Xiaomi'),'./Images/Phone09.jpg',669),
+		('Samsung Galaxy Xcover6',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone08.jpg',669),
+		('Samsung Galaxy Xcover5',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone08.jpg',669),
+		('Samsung Galaxy Tab S20',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone08.jpg',669),
+		('Samsung Galaxy Tab S23',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone08.jpg',669),
+		('Samsung Galaxy Tab A10 Thunderbolt',(select ID from MANUFACTURER where NAME = 'Samsung'),'./Images/Phone08.jpg',669)
