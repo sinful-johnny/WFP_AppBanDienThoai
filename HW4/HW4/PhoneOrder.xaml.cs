b8dfe9b7bd37497c89f861cbbb3f99df93e8054d
ThaiCoder2003 <101780284+ThaiCoder2003@users.noreply.github.com>
2024-03-28 13:58:46 +0700

    PhoneOrder

---
+++ HW4/HW4/PhoneOrder.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW4
+{
+    /// <summary>
+    /// Interaction logic for PhoneOrder.xaml
+    /// </summary>
+    public partial class PhoneOrder : UserControl
+    {
+        public PhoneOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void AddOrder(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateOrder(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void DeleteOrder(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
