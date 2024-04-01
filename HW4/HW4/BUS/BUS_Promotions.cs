a30f6ae2c3492976e31a4721026b17d8379ef2b4
sinful_johnny <megaVNN321123@gmail.com>
2024-04-01 09:56:52 +0700

    Generate data and Design Promotion screen

---
+++ HW4/HW4/BUS/BUS_Promotions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.BUS
+{
+    internal class BUS_Promotions
+    {
+        static public Tuple<DataTable, int, int> GetAllPaging(SqlConnection connection, int page, int rowsPerPage)
+        {
+            return PROMOTIONS_Control.GetAllPaging(connection, page, rowsPerPage);
+        }
+    }
+}
---
+++ HW4/HW4/PromoManagementUserControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW4
+{
+    /// <summary>
+    /// Interaction logic for PromoManagementUserControl.xaml
+    /// </summary>
+    public partial class PromoManagementUserControl : UserControl
+    {
+        SqlConnection _con;
+        int _rowPerPage = 32;
+        int _currentPage = 1;
+        int _pageSize = 10;
+        int totalPages = -1;
+        int totalItems = -1;
+        DataTable _promos;
+        public PromoManagementUserControl(SqlConnection connection)
+        {
+            InitializeComponent();
+            _con = connection;
+        }
+        void LoadData()
+        {
+            try
+            {
+                (_promos, totalItems, totalPages) = PROMOTIONS_Control.GetAllPaging(_con, _currentPage, _rowPerPage);
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+
+            PromoDataGrid.ItemsSource = _promos.DefaultView;
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if(_con.State == ConnectionState.Open)
+            {
+                _con.Close();
+            }
+        }
+    }
+}
