67196a30470750bd34af18ee8b20b50ee4384822
sinful_johnny <megaVNN321123@gmail.com>
2024-04-04 17:44:18 +0700

    Generate mock for table CUSTOMER

---
+++ CUSTOMER_DATA.sql
@@ -0,0 +1,107 @@
+use QLDTHOAI
+go
+
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Todd','Montoya','MALE','083-288-8256','P.O. Box 895, 2486 Proin Rd.','1998-02-03 15:50:29','./Images/blank-pfp.png'),
+  ('Clarke','Knox','MALE','075-722-2595','171 Eu Av.','2002-10-03 05:16:45','./Images/blank-pfp.png'),
+  ('Cherokee','Jordan','MALE','075-008-1560','8771 Amet Avenue','1987-01-20 03:56:44','./Images/blank-pfp.png'),
+  ('Donovan','Melendez','FEMALE','063-181-1705','143-6507 Sapien, Ave','1988-11-21 12:30:53','./Images/blank-pfp.png'),
+  ('Shelby','Sims','FEMALE','046-884-7298','473-6388 Elit. Avenue','1997-09-07 11:28:36','./Images/blank-pfp.png'),
+  ('Lev','Wolf','MALE','015-533-3230','444-6277 Dignissim. Av.','1981-08-18 13:43:59','./Images/blank-pfp.png'),
+  ('Carolyn','Langley','MALE','027-935-4069','Ap #280-3153 Metus. St.','1999-04-03 01:38:28','./Images/blank-pfp.png'),
+  ('Akeem','Holcomb','FEMALE','014-388-1896','497-723 Scelerisque St.','1988-06-09 00:41:14','./Images/blank-pfp.png'),
+  ('Laurel','Hines','FEMALE','015-228-1166','Ap #265-9192 Facilisis, St.','1983-07-09 19:00:21','./Images/blank-pfp.png'),
+  ('Donna','Weiss','MALE','069-288-0452','387-7024 Convallis Rd.','2004-02-17 08:33:11','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Kirk','Shepard','FEMALE','062-984-3804','220-6865 Ut St.','1984-03-11 23:14:19','./Images/blank-pfp.png'),
+  ('Ulric','Burgess','FEMALE','013-846-8426','P.O. Box 302, 3523 Feugiat Ave','2005-10-13 11:07:02','./Images/blank-pfp.png'),
+  ('Yuli','Stephens','MALE','000-640-3388','308-8337 Eget Avenue','1997-09-14 06:31:01','./Images/blank-pfp.png'),
+  ('Meredith','Orr','FEMALE','073-345-3202','7273 Accumsan Ave','1993-08-02 21:21:54','./Images/blank-pfp.png'),
+  ('Lydia','Love','FEMALE','034-314-4229','Ap #454-5555 Feugiat Av.','1979-05-17 15:13:12','./Images/blank-pfp.png'),
+  ('Miranda','Norton','MALE','047-891-5501','7888 Sed St.','1993-04-28 13:31:41','./Images/blank-pfp.png'),
+  ('Ocean','Pickett','MALE','073-584-8185','7408 Lorem Av.','1980-02-04 15:16:44','./Images/blank-pfp.png'),
+  ('Colby','Knight','FEMALE','044-522-1658','Ap #699-1268 Tempor St.','2003-07-07 07:22:27','./Images/blank-pfp.png'),
+  ('Hall','Robinson','FEMALE','098-085-3262','Ap #645-717 Integer Av.','1986-10-29 22:26:50','./Images/blank-pfp.png'),
+  ('Unity','Emerson','FEMALE','068-985-4322','P.O. Box 372, 3961 Aliquam Ave','2008-11-24 01:31:57','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Audrey','Anderson','MALE','082-211-1750','7506 Leo. Rd.','1989-06-23 18:06:23','./Images/blank-pfp.png'),
+  ('Sylvia','Boone','MALE','058-841-1140','P.O. Box 514, 7114 Ac Rd.','1984-02-10 05:46:58','./Images/blank-pfp.png'),
+  ('Hall','Guerrero','MALE','038-618-1542','279-491 Dapibus Av.','1998-11-16 21:29:05','./Images/blank-pfp.png'),
+  ('Drake','Sweeney','MALE','084-425-4490','851-1120 In, Street','2007-06-18 05:48:24','./Images/blank-pfp.png'),
+  ('Rafael','Benjamin','FEMALE','064-245-6326','Ap #861-3168 Rutrum, St.','1994-09-01 05:57:42','./Images/blank-pfp.png'),
+  ('Xyla','Dodson','FEMALE','035-230-4389','Ap #362-8779 Imperdiet Road','1992-09-03 03:10:58','./Images/blank-pfp.png'),
+  ('Omar','Olsen','MALE','028-269-7824','Ap #399-9090 Sed St.','1996-10-12 20:06:19','./Images/blank-pfp.png'),
+  ('Tobias','Lowe','FEMALE','026-623-7781','350-2084 Quisque Rd.','1979-06-17 11:19:36','./Images/blank-pfp.png'),
+  ('Quamar','Spencer','FEMALE','025-880-6836','357-4094 Tellus. Street','1999-12-24 17:37:59','./Images/blank-pfp.png'),
+  ('Maxwell','Hoover','FEMALE','077-038-8760','P.O. Box 955, 194 Faucibus Avenue','2001-09-05 04:38:34','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Reed','Ayala','MALE','010-851-2551','936 Eget Street','1982-10-22 02:59:01','./Images/blank-pfp.png'),
+  ('Victor','Mathis','FEMALE','073-843-5688','833-9649 Id St.','1999-12-11 12:38:21','./Images/blank-pfp.png'),
+  ('Jamalia','Langley','MALE','084-554-6138','Ap #683-9840 Libero. Street','2005-09-22 03:47:41','./Images/blank-pfp.png'),
+  ('Oliver','Pickett','FEMALE','071-064-9325','Ap #691-1859 Lorem Avenue','2001-08-17 02:43:29','./Images/blank-pfp.png'),
+  ('Jesse','Ellis','MALE','066-567-4984','Ap #363-4972 Sed Road','1988-05-28 16:10:59','./Images/blank-pfp.png'),
+  ('Kevyn','Roy','FEMALE','092-827-6707','Ap #283-5730 Tincidunt, Street','1993-06-03 10:48:29','./Images/blank-pfp.png'),
+  ('Gavin','Cline','FEMALE','022-287-8836','Ap #523-3038 Magna Rd.','1986-04-16 09:13:54','./Images/blank-pfp.png'),
+  ('Vance','Foster','FEMALE','054-486-6021','556-5851 Consequat Street','1985-01-21 23:26:02','./Images/blank-pfp.png'),
+  ('Alan','Conner','FEMALE','078-909-4167','P.O. Box 614, 2351 Risus. Road','1995-11-04 20:55:58','./Images/blank-pfp.png'),
+  ('Addison','Ruiz','MALE','069-817-9682','P.O. Box 939, 9330 Venenatis St.','2003-04-01 18:07:21','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Jenna','Lloyd','MALE','033-914-8663','956-1080 Et Avenue','1988-06-21 21:54:06','./Images/blank-pfp.png'),
+  ('Naida','Conway','FEMALE','066-636-2657','Ap #202-6382 Mauris Av.','2000-04-16 14:32:10','./Images/blank-pfp.png'),
+  ('Destiny','Pope','MALE','072-026-3787','Ap #851-7195 Nec Avenue','1997-02-28 07:11:14','./Images/blank-pfp.png'),
+  ('Nadine','David','FEMALE','002-817-2613','2575 Vitae, Street','1989-04-14 21:57:51','./Images/blank-pfp.png'),
+  ('Indira','Burch','FEMALE','048-316-4519','769-5613 Non, St.','1995-02-15 19:52:48','./Images/blank-pfp.png'),
+  ('Jaden','Pierce','FEMALE','003-399-8056','169-5456 Aliquam Avenue','1993-05-07 17:30:48','./Images/blank-pfp.png'),
+  ('Ignatius','England','FEMALE','038-688-2218','627-5173 Arcu. Ave','1985-01-27 22:05:01','./Images/blank-pfp.png'),
+  ('Amery','Leach','FEMALE','032-871-6852','431-6380 Morbi St.','2007-08-01 23:18:04','./Images/blank-pfp.png'),
+  ('Chantale','Frederick','MALE','072-867-7557','851-4760 Lacus. Street','1990-05-18 04:34:30','./Images/blank-pfp.png'),
+  ('Carissa','Gilmore','MALE','053-188-7368','P.O. Box 124, 9281 Arcu. Road','1986-04-12 10:23:42','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Arthur','Hampton','MALE','082-147-4003','P.O. Box 690, 8520 At Rd.','1990-04-22 11:35:46','./Images/blank-pfp.png'),
+  ('Malik','Ashley','FEMALE','078-196-8544','838-3022 Arcu. Avenue','1997-12-02 22:36:23','./Images/blank-pfp.png'),
+  ('Jordan','Blair','FEMALE','030-372-2327','988-415 Urna. Ave','1986-05-04 03:21:32','./Images/blank-pfp.png'),
+  ('Chantale','Dyer','FEMALE','064-268-6454','118-6081 Molestie Road','2007-06-06 22:13:59','./Images/blank-pfp.png'),
+  ('Anne','Abbott','MALE','048-142-1840','3649 Ornare Ave','1996-02-14 09:09:10','./Images/blank-pfp.png'),
+  ('Orson','Bond','MALE','019-388-6676','P.O. Box 531, 7406 Lobortis St.','1990-07-23 09:08:16','./Images/blank-pfp.png'),
+  ('Byron','Kane','FEMALE','025-615-1565','Ap #147-1674 Ut Road','2008-03-24 16:31:16','./Images/blank-pfp.png'),
+  ('Aimee','Allen','MALE','095-825-9332','195-1981 Ante Road','1993-03-17 08:48:31','./Images/blank-pfp.png'),
+  ('Cadman','Ochoa','FEMALE','075-167-3190','P.O. Box 196, 1523 Lacus. Rd.','1987-06-04 23:18:02','./Images/blank-pfp.png'),
+  ('Brynn','Dunlap','MALE','002-787-1337','Ap #785-5451 Arcu. Rd.','2008-07-01 12:39:23','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Xaviera','Newman','FEMALE','091-870-1910','2177 Mauris St.','1987-05-27 08:50:49','./Images/blank-pfp.png'),
+  ('Aladdin','Palmer','FEMALE','069-625-1129','5698 Nec Road','2000-09-30 06:13:30','./Images/blank-pfp.png'),
+  ('Nell','Valentine','MALE','048-169-4166','Ap #473-7172 Dis Avenue','2009-11-16 12:44:00','./Images/blank-pfp.png'),
+  ('Rylee','Moore','MALE','018-906-2578','767-7856 Non Av.','1992-08-17 05:10:37','./Images/blank-pfp.png'),
+  ('Griffin','Waters','MALE','014-755-4166','141-1388 Curabitur Road','2001-06-24 02:42:36','./Images/blank-pfp.png'),
+  ('Juliet','Wynn','FEMALE','004-992-5758','Ap #656-6976 Vehicula Rd.','2005-04-12 06:32:41','./Images/blank-pfp.png'),
+  ('Jack','Reynolds','MALE','007-457-1059','581-9231 Magnis Ave','1995-05-13 00:18:44','./Images/blank-pfp.png'),
+  ('Asher','Clark','MALE','011-392-4238','Ap #108-1276 Velit. Rd.','2008-08-01 02:10:49','./Images/blank-pfp.png'),
+  ('Marcia','Small','MALE','081-147-5626','4706 Nulla Road','1998-01-19 09:20:04','./Images/blank-pfp.png'),
+  ('Odette','Bradshaw','FEMALE','056-168-8687','P.O. Box 641, 3997 Non Rd.','1998-03-01 17:30:29','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Whilemina','Mcknight','MALE','061-548-4361','Ap #848-7256 Nulla Rd.','1990-03-26 20:51:41','./Images/blank-pfp.png'),
+  ('Kasimir','Puckett','FEMALE','091-193-3458','Ap #786-810 Rutrum, Rd.','1999-12-05 20:10:09','./Images/blank-pfp.png'),
+  ('Carter','Strong','FEMALE','054-595-5561','Ap #637-818 Urna Road','1987-12-18 18:01:16','./Images/blank-pfp.png'),
+  ('Calista','Boyd','FEMALE','081-664-8854','453-6187 Cras Ave','1991-06-04 16:01:43','./Images/blank-pfp.png'),
+  ('Willa','Donaldson','MALE','051-495-8542','558-3093 Placerat. St.','1990-07-15 05:57:50','./Images/blank-pfp.png'),
+  ('Chandler','Bowers','FEMALE','058-179-9965','921-7926 Dictum. Rd.','2003-09-05 18:16:20','./Images/blank-pfp.png'),
+  ('Brenda','Baxter','FEMALE','026-397-8957','P.O. Box 152, 9990 At Ave','1990-02-15 06:08:11','./Images/blank-pfp.png'),
+  ('Sydney','Harper','FEMALE','068-116-7272','Ap #675-3037 Nunc St.','1992-09-30 00:18:17','./Images/blank-pfp.png'),
+  ('Alan','Sears','MALE','005-271-4468','Ap #346-6293 Tellus Avenue','1995-10-02 02:18:29','./Images/blank-pfp.png'),
+  ('Laurel','Alston','FEMALE','076-148-6123','P.O. Box 235, 9712 Fusce Street','2003-12-17 06:07:28','./Images/blank-pfp.png');
+INSERT INTO CUSTOMER (FIRSTNAME,LASTNAME,GENDER,PHONENUM,CUS_ADDRESS,DOB,CUS_IMAGE)
+VALUES
+  ('Herman','Moss','MALE','045-161-4311','5865 Duis Avenue','1998-04-02 23:47:13','./Images/blank-pfp.png'),
+  ('Galena','Montoya','MALE','013-408-5620','Ap #240-9060 Pharetra. Rd.','1984-04-18 15:58:02','./Images/blank-pfp.png'),
+  ('Bruno','Pena','MALE','063-183-0811','P.O. Box 149, 886 Libero St.','1997-08-10 15:22:21','./Images/blank-pfp.png'),
+  ('Anne','Hatfield','MALE','063-851-3162','Ap #601-9736 Diam Road','1998-02-27 13:58:22','./Images/blank-pfp.png'),
+  ('Neville','Howell','FEMALE','075-398-8731','Ap #472-6485 A St.','1983-12-15 14:54:55','./Images/blank-pfp.png'),
+  ('Nelle','Hall','MALE','047-764-1765','934-4408 Donec Av.','1991-11-28 19:28:33','./Images/blank-pfp.png');
