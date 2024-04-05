27175ef5e334d50dd21345fc3b9eec1aadb833e3
sinful_johnny <megaVNN321123@gmail.com>
2024-04-06 02:30:04 +0700

    add grant, revoke and renew promotion to Customers tab

--- HW4/HW4/UI/Customers/DataViewDialog.xaml.cs
+++ HW4/HW4/UI/Customers/DataViewDialog.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using HW4.BUS;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +23,70 @@ namespace HW4.UI.Customers
     /// </summary>
     public partial class DataViewDialog : Window
     {
+        SqlConnection _con;
         DataTable data;
-        public DataViewDialog(DataTable data)
+        int _mode;
+        int _CUS_ID;
+        public DataViewDialog(SqlConnection connection, int CUS_ID, int mode)
         {
             InitializeComponent();
-            this.data = data;
+            _con = connection;
+
+            this._mode = mode;
+            this._CUS_ID = CUS_ID;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            if (_mode == Mode.Promotions)
+            {
+                this.data = BUS_Customer.getCustomerPromos(_con, _CUS_ID);
+            }
+            else if (_mode == Mode.Orders)
+            {
+                this.data = BUS_Customer.getCustomerOders(_con, _CUS_ID);
+                RevokePromoMenuClick.IsEnabled = false;
+                RenewPromoMenuClick.IsEnabled = false;
+            }
             DataGridView.ItemsSource = this.data.DefaultView;
         }
+
+        private void RevokePromoMenuClick_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var selectedRow = (DataRowView)DataGridView.SelectedItem;
+                var promoId = (int)selectedRow.Row.ItemArray[1]!;
+                BUS_Customer.revokeCustomerPromo(_con, _CUS_ID, promoId);
+                LoadData();
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private void RenewPromoMenuClick_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var selectedRow = (DataRowView)DataGridView.SelectedItem;
+                var promoId = (int)selectedRow.Row.ItemArray[1]!;
+                BUS_Customer.renewCustomerPromo(_con, _CUS_ID, promoId);
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        internal class Mode
+        {
+            static public int Orders { get { return 1; } }
+            static public int Promotions { get { return 2; } }
+        }
     }
+    
 }
---
+++ HW4/HW4/UI/Customers/GrantPromoDialog.xaml.cs
@@ -0,0 +1,127 @@
+﻿using HW4.BUS;
+using HW4.UI.Promotions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW4.UI.Customers
+{
+    /// <summary>
+    /// Interaction logic for GrantPromoDialog.xaml
+    /// </summary>
+    public partial class GrantPromoDialog : Window
+    {
+        SqlConnection _con;
+        int _rowPerPage = 32;
+        int _currentPage = 1;
+        int _pageSize = 10;
+        int totalPages = -1;
+        int totalItems = -1;
+        DataTable _promos;
+        BindingList<Page> PageOptions = new BindingList<Page>();
+        int CUS_ID;
+        public GrantPromoDialog(SqlConnection connection, int CUS_ID)
+        {
+            InitializeComponent();
+            _con = connection;
+            this.CUS_ID = CUS_ID;
+        }
+        void LoadData()
+        {
+            try
+            {
+                int oldTotalPages = totalPages;
+                (_promos, totalItems, totalPages) = PROMOTIONS_Control.GetAllPaging(_con, _currentPage, _rowPerPage);
+                PromoDataGrid.ItemsSource = _promos.DefaultView;
+                if (oldTotalPages != totalPages)
+                {
+                    PageOptions.Clear();
+                    for (int i = 1; i <= totalPages; i++)
+                    {
+                        PageOptions.Add(new Page()
+                        {
+                            _pageNo = i,
+                            _totalPages = totalPages
+                        });
+                    }
+                    PageSelectComboBox.ItemsSource = PageOptions;
+                    PageSelectComboBox.SelectedIndex = 0;
+                }
+
+                if (_currentPage == 1)
+                {
+                    PrevButton.IsEnabled = false;
+                }
+                else
+                {
+                    PrevButton.IsEnabled = true;
+                }
+
+                if (_currentPage == totalPages)
+                {
+                    NextButton.IsEnabled = false;
+                }
+                else
+                {
+                    NextButton.IsEnabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void PageSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PageSelectComboBox.SelectedItem != null && _currentPage != ((Page)PageSelectComboBox.SelectedItem)._pageNo)
+            {
+                _currentPage = ((Page)PageSelectComboBox.SelectedItem)._pageNo;
+                LoadData();
+            }
+        }
+
+        private void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageSelectComboBox.SelectedItem = PageOptions.FirstOrDefault(x => x._pageNo == _currentPage - 1);
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageSelectComboBox.SelectedItem = PageOptions.FirstOrDefault(x => x._pageNo == _currentPage + 1);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+            PageSelectComboBox.SelectedIndex = 0;
+        }
+
+        private void DataGridCell_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var row = (DataRowView)PromoDataGrid.SelectedItem;
+                var promoId = (int)row.Row.ItemArray[0]!;
+                BUS_Customer.addCustomerPromo(_con, CUS_ID, promoId);
+                DialogResult = true;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(),"Error",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
