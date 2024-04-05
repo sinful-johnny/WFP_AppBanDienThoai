1d0f27cc6985f3dddf10df7bd10772ca2712273d
KyleKennyNelson <147902202+KyleKennyNelson@users.noreply.github.com>
2024-04-05 14:51:57 +0700

    dashboard1

---
+++ HW4/HW4/DashboardScreen.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW4
+{
+    /// <summary>
+    /// Interaction logic for DashboardScreen.xaml
+    /// </summary>
+    public partial class DashboardScreen : Window
+    {
+        public DashboardScreen()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                //this.DragMove();
+            }
+        }
+
+        bool IsMaximized = false;
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    //this.WindowState = WindowState.Normal;
+                    //this.Width = 1280;
+                    //this.Height = 780;
+
+                    bool IsMaximized = false;
+                }
+                else
+                {
+                    //this.WindowState = WindowState.Maximized;
+                    bool IsMaximized = true;
+                }
+            }
+        }
+    }
+}
