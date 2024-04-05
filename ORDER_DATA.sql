5e5e0a3115db9b6d8c1268d34e36a2fc74a78fa1
sinful_johnny <megaVNN321123@gmail.com>
2024-04-05 15:33:05 +0700

    Generate mock infomations for orders

---
+++ ORDER_DATA.sql
@@ -0,0 +1,502 @@
+
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (46,'Sep 8, 2024'),
+  (61,'Jul 29, 2023'),
+  (48,'Jun 10, 2023'),
+  (68,'Jan 5, 2025'),
+  (7,'Jul 21, 2024'),
+  (24,'Feb 12, 2024'),
+  (79,'Mar 24, 2024'),
+  (64,'Jul 20, 2023'),
+  (33,'Apr 2, 2024'),
+  (7,'Mar 26, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (72,'Sep 9, 2023'),
+  (55,'Feb 13, 2024'),
+  (47,'Sep 18, 2023'),
+  (24,'May 3, 2023'),
+  (15,'Jul 20, 2024'),
+  (69,'Nov 23, 2023'),
+  (57,'Apr 20, 2023'),
+  (64,'Jun 5, 2024'),
+  (32,'Dec 1, 2023'),
+  (76,'Aug 30, 2023');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (60,'Jun 26, 2024'),
+  (32,'Apr 14, 2024'),
+  (52,'Apr 5, 2025'),
+  (52,'Mar 20, 2024'),
+  (38,'Oct 5, 2024'),
+  (60,'Mar 31, 2024'),
+  (34,'Apr 2, 2024'),
+  (3,'Jan 3, 2024'),
+  (83,'May 31, 2023'),
+  (5,'Jun 10, 2023');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (24,'May 10, 2024'),
+  (11,'May 10, 2024'),
+  (79,'Jan 31, 2025'),
+  (30,'Mar 3, 2024'),
+  (9,'Sep 28, 2023'),
+  (60,'Mar 25, 2025'),
+  (45,'Feb 26, 2025'),
+  (47,'Sep 16, 2023'),
+  (36,'Oct 27, 2024'),
+  (55,'Aug 27, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (21,'Apr 11, 2023'),
+  (18,'Oct 24, 2024'),
+  (63,'Sep 23, 2023'),
+  (62,'Nov 8, 2024'),
+  (48,'Dec 3, 2023'),
+  (16,'Mar 8, 2025'),
+  (63,'Sep 27, 2024'),
+  (25,'Jun 9, 2023'),
+  (77,'Feb 16, 2024'),
+  (59,'May 13, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (22,'May 12, 2024'),
+  (75,'Nov 4, 2023'),
+  (37,'Sep 5, 2024'),
+  (51,'Dec 7, 2023'),
+  (21,'Jan 15, 2025'),
+  (64,'Sep 17, 2023'),
+  (41,'May 14, 2024'),
+  (84,'Jul 4, 2024'),
+  (86,'Jan 26, 2025'),
+  (33,'Nov 10, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (30,'Oct 3, 2023'),
+  (19,'May 17, 2024'),
+  (3,'Jun 28, 2024'),
+  (13,'Dec 21, 2024'),
+  (44,'Nov 27, 2023'),
+  (22,'Sep 21, 2024'),
+  (70,'Apr 9, 2023'),
+  (36,'Apr 5, 2024'),
+  (74,'Feb 12, 2024'),
+  (74,'Jul 13, 2023');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (46,'Jan 2, 2024'),
+  (68,'Oct 3, 2023'),
+  (20,'Sep 20, 2024'),
+  (84,'Dec 17, 2023'),
+  (49,'Jul 2, 2023'),
+  (8,'May 16, 2023'),
+  (78,'Aug 4, 2024'),
+  (17,'Jul 25, 2023'),
+  (48,'Feb 28, 2024'),
+  (69,'Aug 6, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (49,'Oct 27, 2024'),
+  (73,'Jun 26, 2024'),
+  (83,'Feb 23, 2025'),
+  (41,'Apr 23, 2023'),
+  (25,'Jan 24, 2024'),
+  (43,'Feb 26, 2024'),
+  (22,'Jan 3, 2025'),
+  (40,'Jul 1, 2023'),
+  (4,'Mar 11, 2024'),
+  (20,'Mar 2, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (4,'Jul 2, 2024'),
+  (79,'Jun 16, 2023'),
+  (7,'Dec 13, 2023'),
+  (27,'Sep 3, 2023'),
+  (65,'Jul 24, 2023'),
+  (26,'Aug 27, 2023'),
+  (37,'Jul 4, 2024'),
+  (86,'Jun 18, 2024'),
+  (37,'Mar 24, 2024'),
+  (20,'Oct 25, 2023');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (30,'Oct 29, 2023'),
+  (24,'Apr 12, 2024'),
+  (55,'Jul 11, 2023'),
+  (51,'May 30, 2023'),
+  (65,'Nov 9, 2023'),
+  (34,'Jun 20, 2023'),
+  (50,'Nov 20, 2024'),
+  (59,'Mar 28, 2025'),
+  (34,'Apr 22, 2024'),
+  (71,'Apr 30, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (5,'Jan 30, 2025'),
+  (85,'Oct 2, 2024'),
+  (15,'Sep 16, 2023'),
+  (59,'Feb 15, 2025'),
+  (9,'Feb 19, 2025'),
+  (59,'Jan 13, 2025'),
+  (66,'Jun 17, 2024'),
+  (22,'Jul 16, 2024'),
+  (7,'Jun 4, 2024'),
+  (47,'Feb 4, 2025');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (73,'Mar 22, 2025'),
+  (75,'Feb 26, 2024'),
+  (12,'Sep 13, 2023'),
+  (49,'Mar 28, 2024'),
+  (62,'Oct 20, 2023'),
+  (40,'Jan 20, 2025'),
+  (12,'Mar 29, 2025'),
+  (56,'Jan 22, 2024'),
+  (60,'Jul 2, 2024'),
+  (50,'May 20, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (67,'Aug 13, 2024'),
+  (5,'Mar 2, 2025'),
+  (21,'Feb 21, 2024'),
+  (10,'Dec 24, 2023'),
+  (83,'Mar 17, 2025'),
+  (41,'Jun 6, 2024'),
+  (28,'Oct 23, 2023'),
+  (8,'Oct 19, 2023'),
+  (85,'Feb 2, 2024'),
+  (44,'Jul 23, 2023');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (74,'Aug 12, 2023'),
+  (57,'May 11, 2023'),
+  (15,'Sep 12, 2024'),
+  (58,'Nov 22, 2024'),
+  (60,'Jun 29, 2024'),
+  (56,'Apr 9, 2024'),
+  (6,'Sep 2, 2023'),
+  (61,'Jan 23, 2025'),
+  (56,'May 29, 2023'),
+  (36,'Jun 30, 2024');
+INSERT INTO ORDERS (CUSTOMER_ID,CREATED_DATE)
+VALUES
+  (38,'Sep 27, 2024'),
+  (11,'Mar 20, 2025'),
+  (54,'Mar 7, 2025'),
+  (56,'Mar 23, 2024'),
+  (84,'Apr 17, 2023'),
+  (48,'Oct 22, 2023'),
+  (68,'Jan 7, 2024'),
+  (54,'Apr 17, 2023'),
+  (59,'May 9, 2023'),
+  (8,'Aug 26, 2024');
+
+
+
+
+  INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (33,3,3),
+  (145,24,3),
+  (62,4,4),
+  (62,29,4),
+  (115,26,4),
+  (132,14,4),
+  (65,13,4),
+  (144,22,2),
+  (71,15,4),
+  (91,30,2);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (10,13,2),
+  (24,5,2),
+  (9,26,3),
+  (30,26,4),
+  (29,6,2),
+  (7,17,3),
+  (56,25,5),
+  (131,7,5),
+  (120,3,3),
+  (50,27,2);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (41,31,3),
+  (136,23,3),
+  (24,9,2),
+  (34,24,4),
+  (64,15,2),
+  (23,2,5),
+  (115,28,5),
+  (72,28,2),
+  (90,10,1),
+  (111,19,5);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (154,9,2),
+  (121,27,2),
+  (39,32,2),
+  (79,15,3),
+  (107,10,4),
+  (116,20,1),
+  (139,29,4),
+  (32,18,3),
+  (55,3,5),
+  (88,29,3);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (112,23,4),
+  (113,22,3),
+  (127,29,4),
+  (104,19,3),
+  (51,26,1),
+  (136,26,5),
+  (97,11,4),
+  (123,28,4),
+  (46,18,1),
+  (131,14,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (127,7,3),
+  (146,6,5),
+  (39,27,5),
+  (106,23,2),
+  (52,21,2),
+  (22,7,3),
+  (47,3,4),
+  (54,26,3),
+  (59,28,2),
+  (91,2,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (94,31,2),
+  (65,8,2),
+  (69,15,4),
+  (58,30,1),
+  (154,27,3),
+  (71,27,2),
+  (37,18,5),
+  (46,14,4),
+  (68,31,4),
+  (37,32,1);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (65,16,2),
+  (52,26,5),
+  (156,7,4),
+  (83,7,4),
+  (80,25,3),
+  (139,25,3),
+  (95,2,5),
+  (63,22,5),
+  (112,10,4),
+  (140,28,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (160,11,3),
+  (144,29,4),
+  (92,5,5),
+  (57,26,2),
+  (118,17,4),
+  (60,15,2),
+  (109,15,2),
+  (66,19,3),
+  (84,6,2),
+  (31,19,3);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (125,2,3),
+  (79,28,4),
+  (77,13,4),
+  (57,13,4),
+  (48,29,1),
+  (118,19,3),
+  (33,13,3),
+  (7,18,3),
+  (99,24,3),
+  (97,17,3);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (13,10,2),
+  (53,8,1),
+  (65,10,2),
+  (69,12,4),
+  (93,4,3),
+  (94,23,3),
+  (23,12,4),
+  (93,9,5),
+  (156,2,5),
+  (159,23,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (140,23,2),
+  (72,6,3),
+  (143,22,2),
+  (143,29,3),
+  (84,17,3),
+  (107,6,4),
+  (25,2,3),
+  (128,27,1),
+  (61,10,3),
+  (114,21,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (119,31,4),
+  (125,8,4),
+  (6,25,1),
+  (156,20,4),
+  (79,3,3),
+  (94,10,2),
+  (11,3,2),
+  (44,25,5),
+  (88,23,3),
+  (34,10,2);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (14,26,2),
+  (124,25,5),
+  (40,28,5),
+  (69,14,4),
+  (3,22,3),
+  (99,12,3),
+  (37,19,3),
+  (113,1,2),
+  (62,26,4),
+  (145,2,1);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (43,22,1),
+  (120,8,3),
+  (151,5,2),
+  (63,7,3),
+  (17,32,4),
+  (12,15,4),
+  (57,5,3),
+  (140,22,4),
+  (6,24,5),
+  (3,7,2);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (63,21,4),
+  (18,8,5),
+  (8,10,5),
+  (89,16,4),
+  (115,12,4),
+  (97,15,5),
+  (39,10,3),
+  (68,25,5),
+  (46,32,2),
+  (26,20,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (66,14,3),
+  (15,31,3),
+  (95,3,3),
+  (157,12,4),
+  (123,7,4),
+  (104,2,1),
+  (115,17,2),
+  (39,15,5),
+  (70,16,1),
+  (132,3,4);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (84,22,2),
+  (4,24,5),
+  (115,25,4),
+  (69,31,2),
+  (30,4,5),
+  (116,27,3),
+  (51,15,1),
+  (133,29,4),
+  (154,20,5),
+  (126,15,5);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (63,26,4),
+  (62,5,2),
+  (31,15,2),
+  (111,11,1),
+  (5,24,5),
+  (63,12,2),
+  (2,3,4),
+  (27,16,3),
+  (120,25,5),
+  (60,4,3);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (152,8,2),
+  (63,9,3),
+  (70,24,4),
+  (66,10,2),
+  (67,13,5),
+  (117,16,3),
+  (106,22,4),
+  (42,18,2),
+  (103,20,1),
+  (90,6,2);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (39,31,2),
+  (6,28,4),
+  (146,25,4),
+  (57,9,2),
+  (123,17,2),
+  (20,29,2),
+  (4,17,1),
+  (74,9,3),
+  (75,21,2),
+  (6,6,2);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (133,32,2),
+  (128,28,3),
+  (69,25,3),
+  (103,16,4),
+  (42,22,2),
+  (112,30,2),
+  (114,14,2),
+  (117,4,2),
+  (117,15,2),
+  (81,7,1);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (80,29,5),
+  (40,23,3),
+  (143,6,3),
+  (149,4,5),
+  (88,31,5),
+  (62,15,5),
+  (2,26,2),
+  (115,22,3),
+  (73,27,5),
+  (6,31,3);
+INSERT INTO ORDERS_PHONE (ORDER_ID,PHONE_ID,PHONE_COUNT)
+VALUES
+  (97,29,4),
+  (42,29,1),
+  (147,28,3),
+  (155,12,3),
+  (107,25,4),
+  (91,23,5),
+  (105,11,4),
+  (149,7,3),
+  (104,13,3),
+  (58,13,3);
+
+UPDATE ORDERS_PHONE set TOTAL=PHONE_COUNT * P.PRICE from PHONE as P where P.ID = ORDERS_PHONE.PHONE_ID;
+go
+create view V_ORDER_TOTAL_CAL(ORDER_ID,TOTAL)
+as
+	SELECT OP.ORDER_ID, SUM(OP.TOTAL)
+	from ORDERS_PHONE as OP
+			inner join ORDERS as O on O.ORDER_ID = OP.ORDER_ID
+	group by (OP.ORDER_ID)
+go
+
+update ORDERS set TOTAL = V.TOTAL from V_ORDER_TOTAL_CAL as V where ORDERS.ORDER_ID = V.ORDER_ID;
+
+
+--select OP.ORDER_ID, P.NAME, P.PRICE, OP.PHONE_COUNT, OP.TOTAL
+--from ORDERS_PHONE as OP
+--		inner join PHONE as P on OP.PHONE_ID = P.ID
