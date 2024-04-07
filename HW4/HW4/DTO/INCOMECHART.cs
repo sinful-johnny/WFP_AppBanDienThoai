27693d40580bf7ddc4e6e75bd8f60ea168ced36b
KyleKennyNelson <147902202+KyleKennyNelson@users.noreply.github.com>
2024-04-07 15:12:08 +0700

    siucap

---
+++ HW4/HW4/DTO/INCOMECHART.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.DTO
+{
+    internal class INCOMECHART
+    {
+        public DateTime OrderDate { get; set; }
+        public double TotalPrice { get; set; }
+        public double Profit { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
--- HW4/HW4/ReportChart.xaml.cs
+++ HW4/HW4/ReportChart.xaml.cs
@@ -20,6 +20,7 @@ namespace HW4
     {
 
         private SqlConnection _connection;
+        ObservableCollection<INCOMECHART> _IncomeChartList;
 
         //time handling
         string current = "All time";
@@ -51,144 +52,312 @@ namespace HW4
             this._connection = new SqlConnection(connectionString);
         }
 
-        private void ShowDateRange(object sender, RoutedEventArgs e)
+        private bool ShowDateRange()
         {
             current = "Time Range";
-            start = (DateTime)startDatePicker.SelectedDate;
-            end = (DateTime)endDatePicker.SelectedDate;
-            if (start == null || end == null)
+            if (startDatePicker.SelectedDate == null || endDatePicker.SelectedDate == null)
             {
                 MessageBox.Show("Please choose both day range!", "Missing Limit!", MessageBoxButton.OK);
+                return false;
             }
             else if (start > end)
             {
                 MessageBox.Show("Invalid Dates! Choose again", "Invalid Date!", MessageBoxButton.OK);
+                return false;
             }
+
+            start = (DateTime)startDatePicker.SelectedDate;
+            end = (DateTime)endDatePicker.SelectedDate;
+            return true;
         }
 
-        private ObservableCollection<ORDER> ImcomeHandling(SqlConnection connection, DateTime begin, DateTime end)
+        private ObservableCollection<INCOMECHART> ImcomeHandling(SqlConnection connection, DateTime begin, DateTime end)
         {
-            //var orders = new ObservableCollection<ORDER>();
-            //int skip = (1 - 1) * 10;
-            //int take = 12;
+            var incomechartlist = new ObservableCollection<INCOMECHART>();
 
-            //string sql = """
-            //                  select ORDERS.ORDER_ID, ORDERS.CREATED_DATE, ORDERS.TOTAL
-            //                  WHERE ORDERS.CREATED_DATE BETWEEN @StartDate AND @EndDate
-            //                  ORDER BY ORDERS.ORDER_ID
-            //                  OFFSET @Skip ROWS
-            //                  fetch next @Take rows only
-            //             """;
-            //if (connection.State == ConnectionState.Closed)
-            //{
-            //    connection.Open();
-            //}
-
-            //using (var command = new SqlCommand(sql, connection))
-            //{
-            //    //_connection.Open();
-            //    command.Parameters.Add("@Skip", SqlDbType.Int).Value = skip;
-            //    command.Parameters.Add("@Take", SqlDbType.Int).Value = take;
-            //    command.Parameters.Add("@StartDate", SqlDbType.DateTime).Value = begin;
-            //    command.Parameters.Add("@EndDate", SqlDbType.DateTime).Value = end;
-            //    var reader = command.ExecuteReader();
-
-            //    while (reader.Read())
-            //    {
-            //        int OrderID = (int)reader["ORDER_ID"];
-            //        DateTime OrderDate = (DateTime)reader["CREATED_DATE"];
-            //        double TotalPrice = (double)reader["TOTAL"];
-            //        orders.Add(new ORDER()
-            //        {
-            //            OrderID = OrderID,
-            //            CustomerName = null,
-            //            OrderDate = OrderDate,
-            //            OrderPromos = null,
-            //            PromoList = null,
-            //            TotalPrice = TotalPrice,
-            //            status = null
-            //        });
-            //    }
-            //}
-            //connection.Close();
-            //return orders;
-            var result = ORDERControl.FromDateToDate(connection, 1, 12, begin, end);
-            ObservableCollection<ORDER> orders = result.Item1;
-            return orders;
-        }
-
-        private void chart_Loaded(object sender, RoutedEventArgs e)
-        {
-            var beginDate = new DateTime(2024, 4, 1);
-            var endDate = end;
-            ObservableCollection<ORDER> orders = ImcomeHandling(_connection, beginDate, endDate);
-
-            TimeSpan rangeTimeSpan = endDate.Subtract(beginDate); //declared prior as TimeSpan object
-            double[] incomeEachDay = new double [rangeTimeSpan.Days];
-            DateTime[] timeRange = new DateTime[rangeTimeSpan.Days];
-
-            foreach (var order in orders)
+            string sql = """
+                              select O.ORDER_ID, O.CREATED_DATE, O.TOTAL, (O.TOTAL - P.ORIGINALPRICE*OP.PHONE_COUNT) AS 'PROFIT'
+                              from ORDERS O, ORDERS_PHONE OP, PHONE P
+                              where OP.PHONE_ID = P.ID
+                         	    and OP.ORDER_ID = O.ORDER_ID
+                                and CREATED_DATE BETWEEN @StartDate AND @EndDate
+                         """;
+            if (connection.State == ConnectionState.Closed)
             {
-                if (DateTime.Compare(order.OrderDate, beginDate) >= 0)
+                connection.Open();
+            }
+
+            using (var command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.Add("@StartDate", SqlDbType.DateTime).Value = begin;
+                command.Parameters.Add("@EndDate", SqlDbType.DateTime).Value = end;
+
+                //_connection.Open();
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
                 {
-                    TimeSpan rangeTime = order.OrderDate.Subtract(beginDate);
-                    int Day = rangeTime.Days;
-                    incomeEachDay[Day] += order.TotalPrice;
+                    int OrderID = (int)reader["ORDER_ID"];
+                    DateTime OrderDate = (DateTime)reader["CREATED_DATE"];
+                    double TotalPrice = 0;
+                    double ProfitAll = 0;
+                    if (reader["TOTAL"].GetType() != typeof(DBNull))
+                        TotalPrice = (double)reader["TOTAL"];
+                    if (reader["PROFIT"].GetType() != typeof(DBNull))
+                    {
+                        ProfitAll = (double)reader["PROFIT"];
+                    }
+                    incomechartlist.Add(new INCOMECHART()
+                    {
+                        OrderDate = OrderDate,
+                        TotalPrice = TotalPrice,
+                        Profit = ProfitAll
+                    });
                 }
             }
+            connection.Close();
 
+            return incomechartlist;
+
+
+            //var result = ORDERControl.FromDateToDate(connection, 1, 12, begin, end);
+            //ObservableCollection<ORDER> orders = result.Item1;
+            //return orders;
+        }
+
+        private void declareChartSeries()
+        {
             chart.Series = new SeriesCollection()
             {
                 new LineSeries()
                 {
                     Title = "Doanh thu của cửa hàng",
                     Values = new ChartValues<double>(),
-                    Stroke = Brushes.Red,
-                    Fill = Brushes.Blue,
-                    StrokeDashArray = new DoubleCollection{2}
+                    Stroke = Brushes.DarkRed,
+                    StrokeDashArray = new DoubleCollection{1}
                 },
                 new ColumnSeries()
                 {
                     Title = "Lợi nhuận của cửa hàng",
-                    Values = new ChartValues<decimal> {5, 6, 2, 7, 6},
-                    Stroke = Brushes.Green,
+                    Values = new ChartValues<double>(),
+                    Stroke = Brushes.OrangeRed,
                     StrokeThickness = 2,
-                    Fill = Brushes.Yellow
+                    Fill = Brushes.OrangeRed
                 }
             };
+        }
+
+        private void chart_Loaded(object sender, RoutedEventArgs e)
+        {
+            var beginDate = new DateTime(2024, 4, 1);
+            var endDate = end;
+            List<double> profits;
+            ObservableCollection<INCOMECHART> incomechartlists = new ObservableCollection<INCOMECHART>();
+            incomechartlists = ImcomeHandling(_connection, beginDate, endDate);
+
+            TimeSpan rangeTimeSpan = endDate.Subtract(beginDate); //declared prior as TimeSpan object
+            double[] incomeEachDay = new double [rangeTimeSpan.Days];
+            double[] profitEachDay = new double[rangeTimeSpan.Days];
+            DateTime[] timeRange = new DateTime[rangeTimeSpan.Days];
+
+            foreach (var incomechartlist in incomechartlists)
+            {
+                //if (DateTime.Compare(incomechartlist.OrderDate, beginDate) >= 0)
+                //{
+                //    TimeSpan rangeTime = incomechartlist.OrderDate.Subtract(beginDate);
+                //    int Day = rangeTime.Days;
+                //    incomeEachDay[Day] += incomechartlist.TotalPrice;
+                //}
+                TimeSpan rangeTime = incomechartlist.OrderDate.Subtract(beginDate);
+                int Day = rangeTime.Days;
+                incomeEachDay[Day] += incomechartlist.TotalPrice;
+                profitEachDay[Day] += incomechartlist.Profit;
+            }
+
+            declareChartSeries();
 
             for (int Day = 0; Day < rangeTimeSpan.Days; Day++)
             {
                 chart.Series[0].Values.Add(incomeEachDay[Day]);
+                chart.Series[1].Values.Add(profitEachDay[Day]);
             }
 
             chart.AxisX.Add(new Axis()
             {
                 Labels = new List<string>()
             });
-            
-            for (int i = 0; i < rangeTimeSpan.Days; i++)
+
+            //Store a DateTimes String 
+            var datetimeString = new List<string>();
+            for (int Day = 0; Day < rangeTimeSpan.Days; Day++)
             {
+                timeRange[Day] = beginDate;
+                datetimeString.Add(timeRange[Day].ToString("dd/MM/yyyy"));
                 beginDate = beginDate.AddDays(1);
-                timeRange[i] = beginDate;
-                string haha = timeRange[i].ToString("dd/MM/yyyy");
-                //chart.AxisX[0].Labels.Add(haha);
             }
+            chart.AxisX[0].Labels = datetimeString;
         }
 
         private void ChartWithDateButton_Click(object sender, RoutedEventArgs e)
         {
-            //
+            if (ShowDateRange() == false) return;
+
+            var beginDate = start;
+            var endDate = end;
+
+            List<double> profits;
+            ObservableCollection<INCOMECHART> incomechartlists = new ObservableCollection<INCOMECHART>();
+            incomechartlists = ImcomeHandling(_connection, beginDate, endDate);
+
+            TimeSpan rangeTimeSpan = endDate.Subtract(beginDate); //declared prior as TimeSpan object
+            double[] incomeEachDay = new double[rangeTimeSpan.Days];
+            double[] profitEachDay = new double[rangeTimeSpan.Days];
+            DateTime[] timeRange = new DateTime[rangeTimeSpan.Days];
+
+            foreach (var incomechartlist in incomechartlists)
+            {
+                //if (DateTime.Compare(incomechartlist.OrderDate, beginDate) >= 0)
+                //{
+                //    TimeSpan rangeTime = incomechartlist.OrderDate.Subtract(beginDate);
+                //    int Day = rangeTime.Days;
+                //    incomeEachDay[Day] += incomechartlist.TotalPrice;
+                //}
+                TimeSpan rangeTime = incomechartlist.OrderDate.Subtract(beginDate);
+                int Day = rangeTime.Days;
+                incomeEachDay[Day] += incomechartlist.TotalPrice;
+                profitEachDay[Day] += incomechartlist.Profit;
+            }
+
+            declareChartSeries();
+
+            for (int Day = 0; Day < rangeTimeSpan.Days; Day++)
+            {
+                chart.Series[0].Values.Add(incomeEachDay[Day]);
+                chart.Series[1].Values.Add(profitEachDay[Day]);
+            }
+
+            chart.AxisX.Add(new Axis()
+            {
+                Labels = new List<string>()
+            });
+
+            //Store a DateTimes String 
+            var datetimeString = new List<string>();
+            for (int Day = 0; Day < rangeTimeSpan.Days; Day++)
+            {
+                timeRange[Day] = beginDate;
+                datetimeString.Add(timeRange[Day].ToString("dd/MM/yyyy"));
+                beginDate = beginDate.AddDays(1);
+            }
+            chart.AxisX[0].Labels = datetimeString;
         }
 
         private void ChartWithMonthButton_Click(object sender, RoutedEventArgs e)
         {
-            //
+            if (ShowDateRange() == false) return;
+
+            var beginDate = start;
+            var endDate = end;
+
+            List<double> profits;
+            ObservableCollection<INCOMECHART> incomechartlists = new ObservableCollection<INCOMECHART>();
+            incomechartlists = ImcomeHandling(_connection, beginDate, endDate);
+
+            int rangeMonthSpan = (endDate.Month - beginDate.Month) 
+                                 + 12 * (endDate.Year - beginDate.Year) + 1;
+            double[] incomeEachDay = new double[rangeMonthSpan];
+            double[] profitEachDay = new double[rangeMonthSpan];
+            DateTime[] MonthRange = new DateTime[rangeMonthSpan];
+
+            foreach (var incomechartlist in incomechartlists)
+            {
+                //if (DateTime.Compare(incomechartlist.OrderDate, beginDate) >= 0)
+                //{
+                //    TimeSpan rangeTime = incomechartlist.OrderDate.Subtract(beginDate);
+                //    int Day = rangeTime.Days;
+                //    incomeEachDay[Day] += incomechartlist.TotalPrice;
+                //}
+                int Month = (incomechartlist.OrderDate.Month - beginDate.Month)
+                            + 12 * (incomechartlist.OrderDate.Year - beginDate.Year);
+                incomeEachDay[Month] += incomechartlist.TotalPrice;
+                profitEachDay[Month] += incomechartlist.Profit;
+            }
+
+            declareChartSeries();
+
+            for (int Month = 0; Month < rangeMonthSpan; Month++)
+            {
+                chart.Series[0].Values.Add(incomeEachDay[Month]);
+                chart.Series[1].Values.Add(profitEachDay[Month]);
+            }
+
+            chart.AxisX.Add(new Axis()
+            {
+                Labels = new List<string>()
+            });
+
+            //Store a DateTimes String 
+            var datetimeMonthString = new List<string>();
+            for (int Month = 0; Month < rangeMonthSpan; Month++)
+            {
+                MonthRange[Month] = beginDate;
+                datetimeMonthString.Add(MonthRange[Month].ToString("MM/yyyy"));
+                beginDate = beginDate.AddMonths(1);
+            }
+            chart.AxisX[0].Labels = datetimeMonthString;
         }
 
         private void ChartWithYearButton_Click(object sender, RoutedEventArgs e)
         {
-            //
+            if (ShowDateRange() == false) return;
+
+            var beginDate = start;
+            var endDate = end;
+
+            List<double> profits;
+            ObservableCollection<INCOMECHART> incomechartlists = new ObservableCollection<INCOMECHART>();
+            incomechartlists = ImcomeHandling(_connection, beginDate, endDate);
+
+            int rangeYearSpan = endDate.Year - beginDate.Year + 1;
+            double[] incomeEachDay = new double[rangeYearSpan];
+            double[] profitEachDay = new double[rangeYearSpan];
+            DateTime[] YearRange = new DateTime[rangeYearSpan];
+
+            foreach (var incomechartlist in incomechartlists)
+            {
+                //if (DateTime.Compare(incomechartlist.OrderDate, beginDate) >= 0)
+                //{
+                //    TimeSpan rangeTime = incomechartlist.OrderDate.Subtract(beginDate);
+                //    int Day = rangeTime.Days;
+                //    incomeEachDay[Day] += incomechartlist.TotalPrice;
+                //}
+                int Year = incomechartlist.OrderDate.Year - beginDate.Year;
+                incomeEachDay[Year] += incomechartlist.TotalPrice;
+                profitEachDay[Year] += incomechartlist.Profit;
+            }
+
+            declareChartSeries();
+
+            for (int Year = 0; Year < rangeYearSpan; Year++)
+            {
+                chart.Series[0].Values.Add(incomeEachDay[Year]);
+                chart.Series[1].Values.Add(profitEachDay[Year]);
+            }
+
+            chart.AxisX.Add(new Axis()
+            {
+                Labels = new List<string>()
+            });
+
+            //Store a DateTimes String 
+            var datetimeYearString = new List<string>();
+            for (int Year = 0; Year < rangeYearSpan; Year++)
+            {
+                YearRange[Year] = beginDate;
+                datetimeYearString.Add(YearRange[Year].ToString("MM/yyyy"));
+                beginDate = beginDate.AddMonths(1);
+            }
+            chart.AxisX[0].Labels = datetimeYearString;
         }
     }
 }
