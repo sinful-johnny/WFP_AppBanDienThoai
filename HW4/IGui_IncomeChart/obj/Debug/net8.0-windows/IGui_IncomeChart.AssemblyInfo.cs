64ea87dcaeb742606b053191f87a325a26a239da
KyleKennyNelson <147902202+KyleKennyNelson@users.noreply.github.com>
2024-04-15 16:27:46 +0700

    .

--- HW4/IGui_IncomeChart/obj/Debug/net8.0-windows/IGui_IncomeChart.AssemblyInfo.cs
+++ HW4/IGui_IncomeChart/obj/Debug/net8.0-windows/IGui_IncomeChart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IGui_IncomeChart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19414c32b39ed7d23ac1d947e95b0454142227e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IGui_IncomeChart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IGui_IncomeChart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
